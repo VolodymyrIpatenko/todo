3b4ea3bf0ef2b1da632e631fe156fa1c2bed9cdf
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-07-30 22:10:53 +0300

    c

--- lesson5/build/dashboard/index.js
+++
@@ -1,2 +0,0 @@
-import renderDashboard from './renderDashboard';
-renderDashboard();
--- lesson5/build/profile/index.js
+++
@@ -1,9 +0,0 @@
-import { fetchUser } from './gateway.js';
-import printProfile from './printProfile.js';
-fetchUser('github').then(({
-  name,
-  location
-}) => printProfile({
-  name,
-  location
-}));
