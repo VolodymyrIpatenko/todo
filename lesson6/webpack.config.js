0bc37f2ab51f4a060f3b236cc9066a6449fe6541
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-07-31 14:04:39 +0300

    c

--- lesson6/webpack.config.js
+++ lesson6/webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /.sass$/,
-        use: ['css-loader', 'style-loader', 'sass-loader'],
+        test: /.scss$/,
+        use: ['css-loader', 'style-loader', 'scss-loader'],
       },
       {
         test: /.(jpg|png)$/,
