538011f93afcc2b34d4c34d8ac61a565b9a1d38b
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-07-31 12:43:12 +0300

    с

--- lesson5/task2/webpack.config.js
+++ lesson5/task2/webpack.config.js
@@ -3,10 +3,9 @@ const path = require('path');
 module.exports = {
   entry: {
     profile: './src/profile/index.js',
-    dashboard: './src/dashboard/index.js',
   },
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'vovik'),
   },
 };
