0cf5c089be5e3781fd494d5cfede91b66884284c
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-08-01 11:45:19 +0300

    c

--- lesson6/webpack.config.js
+++ lesson6/webpack.config.js
@@ -1,27 +1,53 @@
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: ['css-loader', 'style-loader', 'sass-loader'],
-      },
-      {
-        test: /.(jpg|png)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              name: '[name].ext',
-              outputPath: 'images',
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /.(jpg|png)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: '[name].ext',
+                outputPath: 'images',
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
+  return config;
 };
